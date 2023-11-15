57c4e44bf4307e843b88ad1fbf33e6c83f2715d2
SuraMahesh97 <suranga@botcalm.com>
2023-11-15 22:38:16 +0530

    add tenmoon updated version

---
+++ script.js
@@ -0,0 +1,60 @@
+ScrollReveal({
+    
+    reset: true,
+    distance: '70px',
+    duration: 2000,
+    delay: 200,
+    // easing: 'ease-in',
+    mobile: true,
+
+});
+
+ScrollReveal().reveal('.t-right', { origin: 'right' });
+ScrollReveal().reveal('.txt1', { origin: 'left',delay:0 });
+ScrollReveal().reveal('.txt2', { origin: 'left',delay:50 });
+ScrollReveal().reveal('.txt3', { origin: 'left',delay:100 });
+ScrollReveal().reveal('.txt4', { origin: 'left',delay:150 });
+
+ScrollReveal().reveal('.txt-top1', { origin: 'top',delay:0 });
+ScrollReveal().reveal('.txt-top2', { origin: 'top',delay:50 });
+ScrollReveal().reveal('.txt-top3', { origin: 'top',delay:100 });
+ScrollReveal().reveal('.txt-top4', { origin: 'top',delay:150 });
+ScrollReveal().reveal('.txt-top5', { origin: 'top',delay:200 });
+
+
+ScrollReveal().reveal('.txt-sub1', { origin: 'top',delay:0 ,distance:'50px'});
+ScrollReveal().reveal('.txt-sub2', { origin: 'top',delay:50 ,distance:'50px'});
+ScrollReveal().reveal('.txt-sub3', { origin: 'top',delay:100 ,distance:'50px'});
+
+ScrollReveal().reveal('.left-1', { origin: 'left',delay:0 ,distance:'50px'});
+ScrollReveal().reveal('.right-1', { origin: 'right',delay:50 ,distance:'50px'});
+ScrollReveal().reveal('.left-2', { origin: 'left',delay:100 ,distance:'50px'});
+ScrollReveal().reveal('.right-2', { origin: 'right',delay:150 ,distance:'50px'});
+
+
+
+// ScrollReveal().reveal('.coatingTypes', { origin: 'top' });
+
+ScrollReveal().reveal('.ins1', { origin: 'left' });
+ScrollReveal().reveal('.ins2', { origin: 'left' });
+
+ScrollReveal().reveal('.ins3', { origin: 'right' });
+ScrollReveal().reveal('.ins4', { origin: 'right' });
+
+
+ScrollReveal().reveal('.package1', { origin: 'left' });
+ScrollReveal().reveal('.package3', { origin: 'right' });
+ScrollReveal().reveal('.package2', { origin: 'bottom' });
+
+
+ScrollReveal().reveal('.feedbach1', { origin: 'left' });
+ScrollReveal().reveal('.feedbach2', { origin: 'left' });
+
+ScrollReveal().reveal('.feedbach3', { origin: 'right' });
+ScrollReveal().reveal('.feedbach4', { origin: 'right' });
+
+
+ScrollReveal().reveal('.bog1', { origin: 'left' });
+ScrollReveal().reveal('.bog3', { origin: 'right' });
+ScrollReveal().reveal('.bog2', { origin: 'bottom' });
+
